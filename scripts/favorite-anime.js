2b3f3404ccce7cd08ceeddfa8d96ec42624062d8
successful-fella <preetjuneja@zingalbox.com>
2020-11-23 20:53:00 +0530

    Skeleton Backend Like Loading on Anime List

--- scripts/favorite-anime.js
+++ scripts/favorite-anime.js
@@ -1,5 +1,18 @@
 function loadPage() {
 	$.get("pages/anime.html", (resp) => {
 		$('#app').html(resp)
+		setTimeout(function() {
+			getAnimeList()
+		}, 1000)
 	})
+}
+
+function getAnimeList() {
+	let resp = '["Karakai no Takagi-san", "Kaguya-sama Love is War", "Adachi to Shimamura", "Clannad", "Re:zero", "KonoSuba", "Kanojo mo Kanojo"]'
+	let list = ''
+	resp = JSON.parse(resp)
+	for(let i = 0; i < resp.length; i++) {
+		list += `<li>${resp[i]}</li>`
+	}
+	$('#anime-list').html(list)
 }
